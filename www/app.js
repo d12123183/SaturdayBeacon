8f8f79f2a1bd255b1f2fa8050d1d48055ef5e5a2
[d12123183] <[d12123183@mydit.ie]>
2016-12-14 00:01:23 +0000

    Added Map

--- www/app.js
+++ www/app.js
@@ -125,16 +125,23 @@ var app = (function()
 
 		var timeNow = Date.now();
 
-        p1 = { x: 1, y: 6, z: 0, r: 1, major: 40851};
-        p2 = { x: 5, y: 3, z: 0, r: 1, major: 54870};
-        p3 = { x: 3, y: 4, z: 0, r: 1, major: 62910};
+        p1 = { x: 1, y: 1, z: 0, r: 1, major: 40851};
+        p2 = { x: 3, y: 1, z: 0, r: 1, major: 54870};
+        p3 = { x: 1, y: 5, z: 0, r: 1, major: 62910};
 
+    /*    p1 = { x: 393, y: 780, z: 0, r: 1, major: 40851};
+        p2 = { x: 389, y: 755, z: 0, r: 1, major: 54870};
+        p3 = { x: 292, y: 734, z: 0, r: 1, major: 62910};*/
+
+        /*p1 = { x: 356, y: 711, z: 0, r: 1, major: 40851};
+        p2 = { x: 393, y: 708, z: 0, r: 1, major: 54870};
+        p3 = { x: 402, y: 764, z: 0, r: 1, major: 62910};*/
 
 		// Update beacon list.
 		$.each(beacons, function(key, beacon)
 		{
 			// Only show beacons that are updated during the last 60 seconds.
-			if (beacon.timeStamp + 600000 > timeNow)
+			if (beacon.timeStamp + 60000 > timeNow)
 			{
 				// Map the RSSI value to a width in percent for the indicator.
 				var rssiWidth = 1; // Used when RSSI is zero or greater.
@@ -160,12 +167,40 @@ var app = (function()
 
                 //p4 = trilaterate(trilateparam1, trilateparam2, trilateparam3);
 
-
+/*
                var S = (Math.pow(p3.x, 2.) - Math.pow(p2.x, 2.) + Math.pow(p3.y, 2.) - Math.pow(p2.y, 2.) + Math.pow(p2.r, 2.) - Math.pow(p3.r, 2.)) / 2.0;
                  var T = (Math.pow(p1.x, 2.) - Math.pow(p2.x, 2.) + Math.pow(p1.y, 2.) - Math.pow(p2.y, 2.) + Math.pow(p2.r, 2.) - Math.pow(p1.r, 2.)) / 2.0 ;
 
                  var py = ((S * (p2.x - p3.x)) - (S * (p2.x - p1.x))) / (((p1.y - p2.y) * (p2.x - p3.x)) - ((p3.y - p2.y) * (p2.x - p1.x)));
-                 var px = ((py * (p1.y -p2.y )) - T) / (p2.x - p1.x);
+                 var px = ((py * (p1.y -p2.y )) - T) / (p2.x - p1.x);*/
+
+
+// second trilateration
+
+                /*p1 = { x: 393, y: 780, z: 0, r: 20, major: 40851};
+                p2 = { x: 389, y: 755, z: 0, r: 10, major: 54870};
+                p3 = { x: 292, y: 734, z: 0, r: 14, major: 62910};*/
+
+                //p1 = { x: 393, y: 780, r: 5};
+                //p2 = { x: 389, y: 755, r: 6};
+                //p3 = { x: 292, y: 734, r: 3};
+
+
+                var xa = p1.x;
+                var ya = p1.y;
+                var xb = p2.x;
+                var yb = p2.y;
+                var xc = p3.x;
+                var yc = p3.y;
+                var ra = p1.r;
+                var rb = p2.r;
+                var rc = p3.r;
+
+                var S = (Math.pow(xc, 2.) - Math.pow(xb, 2.) + Math.pow(yc, 2.) - Math.pow(yb, 2.) + Math.pow(rb, 2.) - Math.pow(rc, 2.)) / 2.0;
+                var T = (Math.pow(xa, 2.) - Math.pow(xb, 2.) + Math.pow(ya, 2.) - Math.pow(yb, 2.) + Math.pow(rb, 2.) - Math.pow(ra, 2.)) / 2.0;
+                var py = ((T * (xb - xc)) - (S * (xb - xa))) / (((ya - yb) * (xb - xc)) - ((yc - yb) * (xb - xa)));
+                var px = ((py * (ya - yb)) - T) / (xb - xa);
+
 
 
                 var element = $(
@@ -178,10 +213,6 @@ var app = (function()
 
 
 
-
-
-
-
                 function errorCB(err) {
                     alert("Error processing SQL: "+err.code);
                     document.getElementById('dbtest').innerHTML='error in db';
@@ -191,29 +222,20 @@ var app = (function()
                 //
                 function successCB() {
 
-
                     document.getElementById('dbtest').innerHTML='success in db';
                 }
 
                 // Cordova is ready
                 //
 
-
-
-
                     //db.transaction(searchQueryDB, errorCB);
                     //db.transaction(querySuccess, errorCB);
-
-
-
-
 
                 function searchQueryDB(tx) {
 
                   //  tx.executeSql('select * from nodes ',[], querySuccess, errorCB);
                     tx.executeSql('select * from nodes where '+ px + ' <=upper_x and ' +py + '<=upper_y and '+ px + ' >=lower_x and ' +py + '>=lower_y',[], querySuccess, errorCB);
                         //'where '+ px + ' <= upper_x AND '+ px + ' >= lower_x AND' +py + '<=upper_y AND '+ px + '>= lower_y',
-
 
                 }
 
@@ -223,7 +245,7 @@ var app = (function()
                     for (var i = 0; i < len; i++) {
                         document.getElementById('dbtest').innerHTML='found something';
                         var tmpArgs=results.rows.item(i).node_location;
-                        document.getElementById('dbtest').innerHTML='curr_location :x:'+px+' y:'+py+ ' area_name '+tmpArgs;
+                        document.getElementById('dbtest').innerHTML='LOCATION :x:'+px+' y:'+py+ ' AREA: '+tmpArgs;
                     }
                      //document.getElementById('dbtest').innerHTML='test in db'+len;
                 }
@@ -273,19 +295,25 @@ var app = (function()
                     return yx(y, x);  // When doing xy(x, y);
                 };
 
-                //Beacon xy
-                //var b1      = xy(1, 2);
-                //var b2      = xy(3, 5);
-                var b3      = xy(px, py);
+                //Beacon static xy
+                var beacon1     = xy(p1.x, p1.y);
+                var beacon2     = xy(p2.x, p2.y);
+				var beacon3 	= xy(p3.x, p3.y);
+
+				// trilaterate x y result
+                var current_location = xy(px, py);
+
                 //var x1 = document.getElementById('curlocationx').innerHTML;
                 //var y1 = document.getElementById('curlocationy').innerHTML;
-                // var b3      = xy(x1, y1);
-                //    var b4     = xy(, document.getElementById('yloc').innerHTML);
+                // var b3	= xy(x1, y1);
+                // var b4   = xy(, document.getElementById('yloc').innerHTML);
+
 
                 // Beacon Markers
-               // L.marker(b1).addTo(map).bindTooltip('Beacon 1');
-               // L.marker(b2).addTo(map).bindTooltip('Beacon 2');
-                 L.marker(b3).addTo(map).bindTooltip('Beacon 3');
+               	L.marker(beacon1).addTo(map).bindTooltip('Beacon 1');
+               	L.marker(beacon2).addTo(map).bindTooltip('Beacon 2');
+                L.marker(beacon3).addTo(map).bindTooltip('Beacon 3');
+				L.marker(current_location).addTo(map).bindTooltip('Your Location');
                 //L.marker(b4).addTo(map).bindTooltip('Beacon 4');
 
                 //L.grid().addTo(map);
